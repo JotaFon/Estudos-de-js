b3f3c2b0b31595e5146445f5b7c308d064b6d3a2
João Victor Fonseca <112357584+JotaFon@users.noreply.github.com>
2023-03-12 18:35:44 -0300

    Add files via upload

---
+++ objetos.js
@@ -0,0 +1,14 @@
+const produto1 = {};
+produto1.nome = 'Celular';
+produto1.preco = 2320.53;
+
+console.log(produto1);
+
+const produto2 = {
+    masculino: {    
+    nome: 'Camisa polo',
+    preco: 79.90
+    }
+}
+
+console.log(produto2)
